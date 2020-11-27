24a3cc71d575c255349371b87130dea1e2c55c83
Jerome Caruso <jerome0caruso@gmail.com>
2020-11-27 17:01:21 -0600

    add heroku

--- server.js
+++ server.js
@@ -53,6 +53,6 @@ app.get('/profile/:id', (req, res) => profile.handleProfile(req, res, db));
 
 app.put('/image', (req, res) =>{ images.handleImages(req, res, db)});
 
-app.listen(3000, () => {
-    console.log('app is running on port!')
+app.listen(process.env.PORT || 3000, () => {
+    console.log(`app is running on port ${process.env.PORT}!`)
 })
